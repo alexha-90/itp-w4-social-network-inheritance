1239f0436df056e78d57c2cbcb6e7c319cf6a1e8
Conor Kennedy <conork369@gmail.com>
2017-06-24 17:05:34 +0000

    social_network

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,48 @@
 
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.following = []
+        self.posts = []
+        
 
     def add_post(self, post):
-        pass
+        post.set_user(self)
+        self.posts.append(post)
+        
+  
+    '''
+    john.add_post(text_post)
+    
+    text_post.user == john  # Important!
+    len(john.posts)
+    >>> 1
+    '''
+
 
     def get_timeline(self):
-        pass
+        timeline = []
+        
+        for friends in self.following:
+            for tweet in friends.posts:
+                timeline.append(tweet)
+        return sorted(timeline, key=lambda tweets: tweets.timestamp, reverse = True)
+    '''
+    [a list of posts from each user that you are following]
+    [user1, user2, user3] <- people your following
+    get_timeline()
+    [user1post1, user1post2, user2post1, user3post1, user3post2]
+    RETURN POSTS SORTED BY TIMESTAMP  -----> https://wiki.python.org/moin/HowTo/Sorting
+    
+    
+    print(john.get_timeline())
+    >>> [<TextPost: Post 3>, <TextPost: Post 2>, <TextPost: Post 1> 
+    '''
+
 
     def follow(self, other):
-        pass
+        self.following.append(other)
+   
+    
--- social_network/posts.py
+++ social_network/posts.py
@@ -1,33 +1,49 @@
 from datetime import datetime
 
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+class Post(object):
+    def __init__(self, text, timestamp=None):
+        self.text = text
+        self.user = None
+        if timestamp is None:
+            self.timestamp = datetime.now()
+        else:
+            self.timestamp = timestamp
+
+    def set_user(self, user):
+        self.user = user
 
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class TextPost(Post):  
+    def __init__(self, text, timestamp=None):
+        super(TextPost, self).__init__(text, timestamp)
+# https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior
+    def __str__(self):
+        return '@' + self.user.first_name + ' ' + self.user.last_name + ':' + ' ' + '"' + self.text + '"' + '\n\t' + '{0:%A}, {0:%b} {0:%d}, {0:%Y}'.format(self.timestamp)
+
+#            '@Kevin Watson: "Sample post text"\n\tTuesday, Jan 10, 2017'
 
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+
+class PicturePost(Post):  
+    def __init__(self, text, image_url, timestamp=None):
+        super(PicturePost, self).__init__(text, timestamp)
+        self.image_url = image_url
+        
+    def __str__(self):
+        return '@' + self.user.first_name + ' ' + self.user.last_name + ':' + ' ' + '"' + self.text + '"' + '\n\t' + self.image_url + '\n\t' + '{0:%A}, {0:%b} {0:%d}, {0:%Y}'.format(self.timestamp)
+
+#            '@Kevin Watson: "Sample post text"\n\thttp://fake-domain.com/images/sample.jpg\n\tTuesday, Jan 10, 2017'
 
 
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class CheckInPost(Post):  # Inherit properly
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        super(CheckInPost, self).__init__(text, timestamp)
+        self.latitude = latitude
+        self.longitude = longitude
+        
+    def __str__(self):
+         return '@' + self.user.first_name + ' Checked In: ' + '"' + self.text + '"' + '\n\t'+ str(self.latitude) + ', ' + str(self.longitude) + '\n\t' + '{0:%A}, {0:%b} {0:%d}, {0:%Y}'.format(self.timestamp)
+
+
+#            '@Kevin Checked In: "Sample post text"\n\t-34.603722, -58.381592\n\tTuesday, Jan 10, 2017'
